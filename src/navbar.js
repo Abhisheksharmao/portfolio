c5fccbbffce6ddfc0da38cb62723b8e12e90a9d8
Abhisheksharmao <105550693+Abhisheksharmao@users.noreply.github.com>
2024-01-16 17:41:23 +0530

    update

--- src/navbar.js
+++ src/navbar.js
@@ -4,8 +4,8 @@ import './css/nav.css'
 export default function Navbar() {
   return (
     <div className='top'>
-      <a href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=abhisheksh0012@gmail.com&tf=1">abhisheksh0012@gmail.com</a>
-      <a href="./Abhishek-frontend-dev.pdf" download="Abhishek-resume"><button>Resume</button></a>
+{/*       <a href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=abhisheksh0012@gmail.com&tf=1">abhisheksh0012@gmail.com</a>
+      <a href="./Abhishek-frontend-dev.pdf" download="Abhishek-resume"><button>Resume</button></a> */}
     </div>
   )
 }
